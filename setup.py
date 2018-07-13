00ef4fcf84d20a5fa4f50b38f7226bfc2d455b59
Dominik Balog <dominicbalog204@gmail.com>
2018-07-13 13:48:33 +0100

    updated setup.py
    added some extra lines for packaging

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(name='tap-redash',
       version='0.0.1',
       description='Singer.io tap for extracting data from the Redash API',
+      url='http://singer.io',
       author='domb16',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_redash'],
+      long_description=open('README.md').read(),
       install_requires=[
           'singer-python>=2.1.4',
           'requests>=2.13.0',
           'pylint',
           'pandas'
-
       ],
       entry_points='''
           [console_scripts]
