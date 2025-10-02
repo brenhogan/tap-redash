d8292f9a20eb377ee2ff09e004637174c9a9a408
Brendan <125093480+brenhogan@users.noreply.github.com>
2025-10-01 23:49:12 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,21 @@
 from setuptools import setup
 
-setup(name='tap-redash',
-      version='0.0.1',
-      description='Singer.io tap for extracting data from the Redash API',
-      url='http://singer.io',
-      author='domb16',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_redash'],
-      long_description=open('README.md').read(),
-      install_requires=[
-          'singer-python>=2.1.4',
-          'requests>=2.13.0',
-          'pylint',
-          'pandas'
-      ],
-      entry_points='''
-          [console_scripts]
-          tap-redash=tap_redash:main
-      ''',
-      )
-
+setup(
+    name='tap-redash',
+    version='0.1.0',
+    description='Singer tap for extracting data from the Redash API',
+    url='https://github.com/hotgluexyz/tap-redash',
+    author='Brendan Hogan',
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    py_modules=['tap_redash'],
+    long_description=open('README.md').read(),
+    install_requires=[
+        'singer-python>=2.1.4',
+        'requests>=2.20.0',
+    ],
+    entry_points='''
+        [console_scripts]
+        tap-redash=tap_redash:main
+    ''',
+    python_requires='>=3.7',
+)
